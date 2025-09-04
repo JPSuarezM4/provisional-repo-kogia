130d22a469415699a6139323ac327b0e66c11b40
juansuarez <jpsuarez1220@gmail.com>
2025-09-04 06:25:15 -0500

    feat: add onSelect functionality to AddBoxPlot and BoxPlotContainer components

--- frontend/src/components/BoxPlotContainer.jsx
+++ frontend/src/components/BoxPlotContainer.jsx
@@ -1,17 +1,20 @@
 import AddBoxPlot from './AddBoxPlot';
 import PropTypes from 'prop-types';
 
-const BoxPlotContainer = ({ charts, onDeleteChart }) => {
+const BoxPlotContainer = ({ charts, onDeleteChart, onSelectBoxPlot }) => {
     return (
         <>
-            {charts.map((chartConfig, index) => (
+            {charts.map((chartConfig) => (
                 <AddBoxPlot
-                    key={index}
+                    key={chartConfig.id}
                     nodo_id={chartConfig.nodo_id}
                     dispositivo_id={chartConfig.dispositivo_id}
                     sensor_id={chartConfig.sensor_id}
                     medida_id={chartConfig.medida_id}
                     onDelete={() => onDeleteChart(chartConfig.id)}
+                    onSelect={(selected, processedData, processingType) =>
+                        onSelectBoxPlot(chartConfig.id, selected, processedData, processingType)
+                    }
                 />
             ))}
         </>
@@ -21,6 +24,7 @@ const BoxPlotContainer = ({ charts, onDeleteChart }) => {
 BoxPlotContainer.propTypes = {
     charts: PropTypes.arrayOf(
         PropTypes.shape({
+            id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
             nodo_id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
             dispositivo_id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
             sensor_id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
@@ -28,6 +32,7 @@ BoxPlotContainer.propTypes = {
         })
     ).isRequired,
     onDeleteChart: PropTypes.func.isRequired,
+    onSelectBoxPlot: PropTypes.func.isRequired,
 };
 
 export default BoxPlotContainer;
